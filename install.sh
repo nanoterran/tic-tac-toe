a238fa50bc70055007aaa99479d7a2d8f2a1f5df
nanoterran <nanoterran@yahoo.com>
2020-03-08 21:33:26 -0700

    Install dependencies for game

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+WHITE='\033[0;37m'
+
+
+install_dependencies() {
+  sudo apt-get install -y build-essential
+  sudo apt-get install -y luarocks
+  sudo luarocks install busted
+  sudo luarocks install luacov
+}
+
+echo -e "${GREEN}[+] Installing Dependencies${WHITE}"
+
+install_dependencies
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}[+] Done Installing Dependencies${WHITE}"
+else
+    echo -e "${RED}[-] Error Installing Dependencies${WHITE}"
+fi
