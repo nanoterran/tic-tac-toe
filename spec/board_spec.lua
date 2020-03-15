65e04147d0dcd127adcf8ab10990ed1e34c9b8e0
Javier <nanoterran@yahoo.com>
2020-03-15 15:53:13 -0700

    should pass model to board

--- spec/board_spec.lua
+++ spec/board_spec.lua
@@ -1,13 +1,20 @@
 describe('board', function()
+	local mach = require 'mach'
   local Board = require 'board'
   local board
+
+  local fake_model_table = {
+		read = function() end,
+		write = function() end
+  }
+  fake_model = mach.mock_table(fake_model_table, 'fake_model_table')
 
   local function given_the_board_is_initialized_with(config)
     board = Board(config)
   end
 
   it('should initialize a board with empty cells', function()
-    given_the_board_is_initialized_with({})
+    given_the_board_is_initialized_with({ model = fake_model })
 
     local expected = {}
     for row = 1, 3 do
@@ -23,7 +30,7 @@ describe('board', function()
   end)
 
   it('should be able to get a board cell', function()
-    given_the_board_is_initialized_with({})
+    given_the_board_is_initialized_with({ model = fake_model })
 
     actual = board.get_cell(2, 2)
 
@@ -31,11 +38,17 @@ describe('board', function()
   end)
 
   it('should be able to set a board cell', function()
-    given_the_board_is_initialized_with({})
+    given_the_board_is_initialized_with({ model = fake_model })
 
     board.set_cell(1, 2, 'cross')
     actual = board.get_cell(1, 2)
 
     assert.are.equal('cross', actual)
+  end)
+
+  it('should return error when a model is not provide', function()
+    assert.has_error(function() 
+      given_the_board_is_initialized_with({})
+    end, 'No Data Model provided')
   end)
 end)
--- src/board.lua
+++ src/board.lua
@@ -1,5 +1,6 @@
 return function(config)
   local board = {}
+  local model
 
   local function initialize_board()
     for row = 1, 3 do
@@ -21,6 +22,9 @@ return function(config)
   local function set_cell(row, column, value)
     board[row][column] = value
   end
+
+  model = config.model
+  assert(model ~= nil, 'No Data Model provided')
 
   initialize_board()
 
