a6588d82ae566fdf18ce7cc6ab68dc4721eba449
Javier <nanoterran@yahoo.com>
2020-03-15 16:09:44 -0700

    add update to board

--- spec/board_spec.lua
+++ spec/board_spec.lua
@@ -9,6 +9,17 @@ describe('board', function()
   }
   fake_model = mach.mock_table(fake_model_table, 'fake_model_table')
 
+  local function get_empty_board()
+    local empty_board = {}
+    for row = 1, 3 do
+      empty_board[row] = {}
+      for column = 1, 3 do
+        empty_board[row][column] = ' '
+      end
+    end
+    return empty_board
+  end
+
   local function given_the_board_is_initialized_with(config)
     board = Board(config)
   end
@@ -16,14 +27,7 @@ describe('board', function()
   it('should initialize a board with empty cells', function()
     given_the_board_is_initialized_with({ model = fake_model })
 
-    local expected = {}
-    for row = 1, 3 do
-      expected[row] = {}
-      for column = 1, 3 do
-        expected[row][column] = ' '
-      end
-    end
-
+    local expected = get_empty_board()
     actual = board.get_board()
 
     assert.are.same(expected, actual)
@@ -40,15 +44,31 @@ describe('board', function()
   it('should be able to set a board cell', function()
     given_the_board_is_initialized_with({ model = fake_model })
 
-    board.set_cell(1, 2, 'cross')
+    board.set_cell(1, 2, 'X')
     actual = board.get_cell(1, 2)
 
-    assert.are.equal('cross', actual)
+    assert.are.equal('X', actual)
   end)
 
   it('should return error when a model is not provide', function()
     assert.has_error(function() 
       given_the_board_is_initialized_with({})
     end, 'No Data Model provided')
+  end)
+
+  it('should allow to update the board with an input', function()
+    given_the_board_is_initialized_with({ model = fake_model })
+
+    fake_model.read.should_be_called_with('input')
+      .and_will_return({ row = 2, column = 2 })
+      .and_then(fake_model.read.should_be_called_with('current_player')
+      .and_will_return('O'))
+      .when(function()
+        board.update()
+      end)
+
+      local actual = board.get_cell(2, 2)
+
+      assert.are.equal('O', actual)
   end)
 end)
--- src/board.lua
+++ src/board.lua
@@ -23,6 +23,13 @@ return function(config)
     board[row][column] = value
   end
 
+  local function update()
+    local input = model.read('input')
+    local current_player = model.read('current_player')
+
+    set_cell(input.row, input.column, current_player)
+  end
+
   model = config.model
   assert(model ~= nil, 'No Data Model provided')
 
@@ -30,6 +37,7 @@ return function(config)
 
   return {
     get_board = get_board,
+    update = update,
     get_cell = get_cell,
     set_cell = set_cell
   }
