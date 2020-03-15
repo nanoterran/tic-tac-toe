dfc4f4451717baa467311a591ac543f6a8fbe5a2
Javier <nanoterran@yahoo.com>
2020-03-14 18:56:04 -0700

    Adding more tests to board

--- spec/board_spec.lua
+++ spec/board_spec.lua
@@ -9,10 +9,37 @@ describe('board', function()
   it('should initialize a board with empty cells', function()
     given_the_board_is_initialized_with({})
 
-    local expected_board = {}
-    for i = 1, 10 do expected_board[i] = ' ' end
+    local expected = {}
+    for row = 1, 3 do
+      expected[row] = {}
+      for column = 1, 3 do
+        expected[row][column] = ' '
+      end
+    end
 
-    assert.are.same(expected_board, board.get_board())
+    actual = board.get_board()
+
+    assert.are.same(expected, actual)
   end)
+
+  it('should be able to get a board cell', function()
+    given_the_board_is_initialized_with({})
+
+    actual = board.get_cell(2, 2)
+
+    assert.are.equal(' ', actual)
+  end)
+
+  it('should be able to set a board cell', function()
+    given_the_board_is_initialized_with({})
+
+    board.set_cell(1, 2, 'cross')
+    actual = board.get_cell(1, 2)
+
+    assert.are.equal('cross', actual)
+  end)
+
+  -- TODO: Test to do
+  -- should set a board cell
 
 end)
--- src/board.lua
+++ src/board.lua
@@ -2,8 +2,11 @@ return function(config)
   local board = {}
 
   local function initialize_board()
-    for i = 1, 10 do
-      board[i] = ' '
+    for row = 1, 3 do
+      board[row] = {}
+      for column = 1, 3 do
+        board[row][column] = ' '
+      end
     end 
   end
 
@@ -11,9 +14,19 @@ return function(config)
     return board
   end
 
+  local function get_cell(row, column)
+    return board[row][column]
+  end
+
+  local function set_cell(row, column, value)
+    board[row][column] = value
+  end
+
   initialize_board()
 
   return {
-    get_board = get_board
+    get_board = get_board,
+    get_cell = get_cell,
+    set_cell = set_cell
   }
 end
